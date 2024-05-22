391355ed6ce74419543adc117c4bfc7a55321d75
dc <3242627907@qq.com>
2024-05-22 16:55:49 +0800

    哨卫生成更改

--- src/main/java/liuLZmod/monsters/abstracrt/abstract_llz_jiXie.java
+++ src/main/java/liuLZmod/monsters/abstracrt/abstract_llz_jiXie.java
@@ -5,15 +5,16 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * 机械的虚类
  */
-public abstract class abstract_llz_jixie extends CustomMonster {
-
+public abstract class abstract_llz_jiXie extends CustomMonster {
 
     /**
      * 是否被销毁
@@ -24,8 +25,10 @@ public abstract class abstract_llz_jixie extends CustomMonster {
      * 攻击目标
      */
     public AbstractCreature target = null;
-
-
+    /**
+     * 是否拥有机械能力标志
+     */
+    public static boolean isHasJiXiePower = false;
     /**
      * 充能
      */
@@ -33,44 +36,29 @@ public abstract class abstract_llz_jixie extends CustomMonster {
     /**
      * 触发能量
      */
-    public static int maxEnergy = 6;
+    public static int maxEnergy = 1;
+    /**
+     * 每个派生的机械类，都需要向其注册
+     */
+    public static List<abstract_llz_jiXie> jiXie_list = new ArrayList<>();
 
-    public abstract_llz_jixie(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl, float offsetX, float offsetY) {
+    public abstract_llz_jiXie(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl, float offsetX, float offsetY) {
         super(name, id, maxHealth, hb_x, hb_y, hb_w, hb_h, imgUrl, offsetX, offsetY);
     }
 
-    public abstract_llz_jixie(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl, float offsetX, float offsetY, boolean ignoreBlights) {
+    public abstract_llz_jiXie(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl, float offsetX, float offsetY, boolean ignoreBlights) {
         super(name, id, maxHealth, hb_x, hb_y, hb_w, hb_h, imgUrl, offsetX, offsetY, ignoreBlights);
     }
 
-    public abstract_llz_jixie(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl) {
+    public abstract_llz_jiXie(String name, String id, int maxHealth, float hb_x, float hb_y, float hb_w, float hb_h, String imgUrl) {
         super(name, id, maxHealth, hb_x, hb_y, hb_w, hb_h, imgUrl);
     }
 
     /**
-     * 返回一个随机的敌方目标
+     * 返回攻击目标
      */
     public AbstractMonster getTarget() {
-        if (AbstractDungeon.getMonsters() == null) {
-            return null;
-        } else {
-            List<AbstractMonster> monsters = new ArrayList();
-            Iterator var2 = AbstractDungeon.getMonsters().monsters.iterator();
-
-            while (var2.hasNext()) {
-                AbstractMonster m = (AbstractMonster) var2.next();
-                if (!m.escaped && !m.isDying && !m.isDead && m.currentHealth > 0 && !(m instanceof abstract_llz_jixie)) {
-                    monsters.add(m);
-                }
-            }
-
-            if (monsters.size() == 0) {
-                return null;
-            } else {
-                int rnd = AbstractDungeon.monsterRng.random(0, monsters.size() - 1);
-                return (AbstractMonster) monsters.get(rnd);
-            }
-        }
+        return null;
     }
 
     public void update() {
@@ -78,22 +66,66 @@ public abstract class abstract_llz_jixie extends CustomMonster {
     }
 
     /**
-     * 能量增加
+     * 机械注册函数
      */
-    public void addEnergy(int num) {
-        energy += num;
-        if (energy >= maxEnergy) {
-            action();
+    public static void addJiXie(abstract_llz_jiXie jiXie) {
+        if (!isHasJiXiePower) {
+            isHasJiXiePower = true;
+        }
+        jiXie_list.add(jiXie);
+    }
+
+    /**
+     * 继承类必须重写这个方法。
+     * （派生类）增加充能，并检测充能是否触发条件。
+     * （基类） 调用注册类的 addEnergy 方法。
+     * num：充能层数
+     */
+    public static void addEnergy(int num) {
+        Class<?> cls;
+        Method method;
+        for (abstract_llz_jiXie jiXie : jiXie_list) {
+            cls = jiXie.getClass();
+            try {
+                method = cls.getMethod("addEnergy", int.class);
+                method.invoke(num);
+            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
     /**
-     * 机械行动函数
-     * 条件:充能达到触发条件 或 主动触发
+     * 派生类必须重写的重置机械列表方法
      */
-    public void action() {
+    public static void clear() {
+    }
+
+    /**
+     * 清空机械列表
+     */
+    public static void clearJiXie() {
+        // 清空渲染机械列表
+        MonsterGroup minions = new MonsterGroup(new AbstractMonster[0]);
+        minions.monsters.removeIf(Objects::isNull);
+        testPatch.f_minions.set(AbstractDungeon.player, minions);
+
+        // 通过反射，调用注册的过的所有名为 clear 的方法
+        Class<?> cls;
+        Method method;
+        for (abstract_llz_jiXie jiXie : jiXie_list) {
+            cls = jiXie.getClass();
+            try {
+                method = cls.getMethod("clear");
+                method.invoke(null);
+            } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 
     }
+
 }
 
 
