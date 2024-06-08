c335c699880a8de43afceafe6ed47e266c67d2a7
ikun0077 <3070673140@qq.com>
2024-06-08 20:46:28 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/liuLZmod/cards/llz_beny.java
+++ src/main/java/liuLZmod/cards/llz_beny.java
@@ -15,7 +15,7 @@ public class llz_beny extends CustomCard {
     public static final String ID = "llz_beny";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "ModliuLZ/img/cards_2/fy.png";
+    private static final String IMG_PATH = "ModliuLZ/img/cards_2/beny.png";
     private static final int COST = 0;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.SKILL;
