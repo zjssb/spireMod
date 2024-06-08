d552edbbc377baac26a6da5108b1f74c731be183
ikun0077 <3070673140@qq.com>
2024-06-08 20:48:53 +0800

    Merge remote-tracking branch 'origin/master'

--- src/main/java/liuLZmod/cards/llz_jinmyq.java
+++ src/main/java/liuLZmod/cards/llz_jinmyq.java
@@ -23,7 +23,7 @@ public class llz_jinmyq extends CustomCard {
     public static final String ID = "llz_jinmyq";
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(ID);
     private static final String NAME = CARD_STRINGS.NAME;
-    private static final String IMG_PATH = "ModliuLZ/img/cards_2/husz.png";
+    private static final String IMG_PATH = "ModliuLZ/img/cards_2/jinmyq.png";
     private static final int COST = 1;
     private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
     private static final CardType TYPE = CardType.POWER;
