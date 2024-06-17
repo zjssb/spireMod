c964307743e7c193826b72362efd318efec5b2a3
dc <3242627907@qq.com>
2024-06-17 20:55:49 +0800

    卡牌补丁

---
+++ src/main/java/liuLZmod/monster/abstracrt/abstract_llz_jiXie.java
@@ -0,0 +1,7 @@
+package liuLZmod.monster.abstracrt;
+
+/**
+ * abstract_llz_jiXie:
+ */
+public class abstract_llz_jiXie {
+}
---
+++ src/main/java/liuLZmod/patches/Card/CardPatch.java
@@ -0,0 +1,33 @@
+package liuLZmod.patches.Card;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import liuLZmod.monster.abstracrt.abstract_llz_jiXie;
+
+/**
+ * CardPatches:
+ * 卡牌补丁。
+ * 对卡牌 use方法进行注入。
+ * 效果：在卡牌使用后，执行机械充能函数
+ */
+public class CardPatch {
+
+    @SpirePatch(clz = AbstractPlayer.class, method = "useCard")
+    public static class usePatch {
+        public usePatch() {
+        }
+        public static void Postfix(AbstractPlayer player, AbstractCard c, AbstractMonster monster, int energyOnUse) {
+            if(c.purgeOnUse){
+                return;
+            }
+            int cost = c.costForTurn;
+            if(c.cost == -1){
+                // 对x牌单独判断
+                cost = c.energyOnUse;
+            }
+            abstract_llz_jiXie.addEnergy(cost);
+        }
+    }
+}
